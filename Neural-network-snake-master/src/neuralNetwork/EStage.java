2cc0b9351d0622713bbd75a1df93e4dc538db539
root <jordan.daddeo@gmail.com>
2018-02-23 18:57:32 -0500

    Removed sigmoid

--- Neural-network-snake-master/src/neuralNetwork/EStage.java
+++ Neural-network-snake-master/src/neuralNetwork/EStage.java
@@ -27,13 +27,10 @@ public class EStage {
 				sum += coeffs[i][j]*prev.output[j];
 			}
 			sum += coeffs[i][coeffs[0].length-1]*signalMultiplier;  //constant bias
-			output[i] = sigmoid(sum);
+			output[i] = sum;
 		}
 	}
-	public static double sigmoid(double x) {
-		//TODO: wtf is sigmoid
-		return 0.0;
-	}
+	
 	public String toString(){
 		String k = "[";
 		for (int i = 0; i< coeffs.length; i++){
