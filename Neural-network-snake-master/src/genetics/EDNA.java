b5177422a0cff101ebebad5c014d5e60a686c892
BlueTinge <totallyawesomecraft@gmail.com>
2018-02-23 18:44:22 -0500

    started to write op crossover

--- Neural-network-snake-master/src/genetics/EDNA.java
+++ Neural-network-snake-master/src/genetics/EDNA.java
@@ -12,48 +12,36 @@ public class EDNA {
 	 */
 	public Random random = new Random();
 	public byte op[];
-	public byte sp[];
+	public byte sigma;
+	
+	public static final double alpha = 0.5;
 	
 	public EDNA(boolean random, int size){
 		op = new byte[size];
-		sp = new byte[size];
+		//sp = new byte[size];
 		
 		for (int i = 0; i < op.length; i++){
 			op[i] = random?(byte)Math.floor(Math.random()*256d):0;
-			sp[i] = random?(byte)Math.floor(Math.random()*256d):0;
 		}
+		sigma = random?(byte)Math.floor(Math.random()*256d):0;
 	}
 	/**
 	 * Crossover function which combines this DNA with another DNA object.
-	 * Process is done byte-wise and a gaussian noise is added to each byte-value 
-	 * Bits flip according to mutation probability
+	 * Process is done byte-wise via whole arithmetic crossover
+	 * 
 	 */
-	public EDNA crossover(EDNA other){  //byte-wise, noise applied to each value
+	public EDNA crossoverWithMutation(EDNA other){  //byte-wise, noise applied to each value
 		EDNA newdna = new EDNA(false, op.length);
-		int numswaps = op.length/10; 
-		int swaps[] = new int[numswaps+1];
-		for (int i = 0; i < swaps.length-1; i++){
-			swaps[i] = (int)Math.floor(Math.random()*op.length);
+
+		//whole arithmetic crossover
+		for(int i = 0; i < other.op.length; i++){
+			
 		}
-		swaps[numswaps] = op.length;  //save last
-		Arrays.sort(swaps);
-		int swapidx = 0;
-		boolean that = true;
-		for (int i = 0; i < op.length; i++){
-			if (i >= swaps[swapidx]){
-				swapidx++;
-				that = !that;
-			}
-			byte d = 0;
-			if (that){
-				d = this.op[i];
-			}
-			else {
-				d = other.op[i];
-			}
-			d += (byte)(random.nextGaussian()*256);
-			newdna.op[i] = d;
-		}
+		
+		//mutation of op
+		
+		//mutation of sigma
+		
 		return newdna;
 	}
 }
