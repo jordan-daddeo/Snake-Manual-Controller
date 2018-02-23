6645e0eddb1b5d20c1b6fcfa1cd8378c346d39f2
root <jordan.daddeo@gmail.com>
2018-02-23 18:56:54 -0500

    Changed game window title

--- Neural-network-snake-master/src/main/MainWindow.java
+++ Neural-network-snake-master/src/main/MainWindow.java
@@ -20,7 +20,7 @@ public class MainWindow extends JFrame {
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setSize( 1000, 600);
 		setExtendedState(MAXIMIZED_BOTH);
-		setTitle("Neural Net Snake Genetic Algorithm Except Better");
+		setTitle("Neural Net Snake Genetic Algorithm Except Better Because It's Evolutionary Strategies");
 		KeyboardListener keyb = new KeyboardListener();
 		addKeyListener(keyb);
 		add(new ESGame(keyb));
